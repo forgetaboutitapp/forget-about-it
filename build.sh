fb65408c92ee645b507a2760eb09dd288672dc82
Your Name <you@example.com>
2025-05-13 22:24:07 -0700

    fixed build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ pwd
 go run ./generate.go
 cd frontend
 dart run build_runner build -d
+cd ../server
+go run ./generate.go
+cd ../frontend
 flutter build web --release
 mkdir ../server/web -p
 cp ./build/web ../server/web -rf
