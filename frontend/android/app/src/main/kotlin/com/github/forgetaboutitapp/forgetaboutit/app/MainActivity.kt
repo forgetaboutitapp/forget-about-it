20693cbc58fc04d75ab19bd19ed410d4235f7f55
Your Name <you@example.com>
2026-05-11 21:16:06 -0700

    allow a createMulticastLock to stay open

--- frontend/android/app/src/main/kotlin/com/github/forgetaboutitapp/forgetaboutit/app/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/github/forgetaboutitapp/forgetaboutit/app/MainActivity.kt
@@ -1,5 +1,23 @@
 package com.github.forgetaboutitapp.forgetaboutit.app
 
+import android.content.Context
+import android.net.wifi.WifiManager
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private var multicastLock: WifiManager.MulticastLock? = null
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        val wifi = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        multicastLock = wifi.createMulticastLock("forgetaboutitMdns")
+        multicastLock?.setReferenceCounted(true)
+        multicastLock?.acquire()
+    }
+
+    override fun onDestroy() {
+        multicastLock?.release()
+        super.onDestroy()
+    }
+}
